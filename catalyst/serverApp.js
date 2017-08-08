ae261515126dec6cac5851e951db87dba4e523d6
jaggia <anadi.jaggia@gmail.com>
2017-08-08 00:11:41 -0400

    registering on back end :)

--- catalyst/serverApp.js
+++ catalyst/serverApp.js
@@ -1,25 +1,37 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const path = require('path');
-const config = require('./serverSide/config/database');
+/* ===================
+   Import Node Modules
+=================== */
+const express = require('express'); // Fast, unopinionated, minimalist web framework for node.
+const app = express(); // Initiate Express Application
+const router = express.Router(); // Creates a new router object.
+const mongoose = require('mongoose'); // Node Tool for MongoDB
+const config = require('./serverSide/config/database'); // Mongoose Config
+const path = require('path'); // NodeJS Package for file paths
+const authentication = require('./routes/authentication')(router); // Import Authentication Routes
+const bodyParser = require('body-parser'); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 
+// Database Connection
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
   if (err) {
-    console.log("Cant connect to DB " , err);
+    console.log('Could NOT connect to database: ', err);
   } else {
-    console.log(config.secret);
-    console.log("Connected to" , config.db);
+    console.log('Connected to database: ' + config.db);
   }
 });
 
-app.use(express.static(__dirname + '/dist/'));
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(express.static(__dirname + '/client/dist/')); // Provide static directory for frontend
+app.use('/authentication', authentication);
 
+// Connect server to Angular 2 Index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 
-app.listen('8080', () => {
-  console.log('app is listening');
+// Start Server: Listen on port 8080
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
 });
