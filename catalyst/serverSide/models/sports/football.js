6198db5a7a8af4e30d3b045385ac925996d2ee79
jaggia <anadi.jaggia@gmail.com>
2017-10-22 15:59:05 -0400

    recruit/athlete working

--- catalyst/serverSide/models/sports/football.js
+++ catalyst/serverSide/models/sports/football.js
@@ -6,7 +6,41 @@ mongoose.Promise = global.Promise; // Configure Mongoose Promises
 const Schema = mongoose.Schema; // Import Schema from Mongoose
 
 const footballSchema = new Schema({
-  fortyDash : { type  : Number, required : true }
+  nonCompetitive : {
+    proShuttle : {
+      segment1 : {type  : Number, required : true},
+      segment2 : {type  : Number, required : true},
+      segment3 : {type  : Number, required : true},
+      segment4 : {type  : Number, required : true}
+    },
+    threeConeDrill : {
+      segment1 : {type  : Number, required : true},
+      segment2 : {type  : Number, required : true},
+      segment3 : {type  : Number, required : true},
+      segment4 : {type  : Number, required : true}
+    },
+    dashes : {
+      fiveYard : {type  : Number, required : true},
+      tenYard : {type  : Number, required : true},
+      twentyYard : {type  : Number, required : true},
+      thirtyYard : {type  : Number, required : true},
+      fortyYard : {type  : Number, required : true}
+    },
+    sixtyYardConeDrill : {
+      segment1 : {type  : Number, required : true},
+      segment2 : {type  : Number, required : true},
+      segment3 : {type  : Number, required : true},
+      segment4 : {type  : Number, required : true}
+    },
+  },
+  inGameMetrics : {
+    WR : {
+      snaps : {
+        to5Yard : {type : Number, required : true},
+        to10Yard : {type : Number, required : true}
+      }
+    }
+  }
 });
 
 module.exports = mongoose.model('Football', footballSchema);
